d5ee95c6a2610365e24543a1058a981702cc17a7
safimiazi <mohibullamiazi@gmail.com>
2024-01-30 13:31:08 +0600

    meta title added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <main>
+        <main className="font-normal">
           {/* ?Header */}
           {children}
           {/* ?Footer */}
