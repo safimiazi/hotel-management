d5d8caa9f8823e48cbccbc90428e82a819b0474f
safimiazi <mohibullamiazi@gmail.com>
2024-01-31 00:05:55 +0600

    form function added

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,18 +1,23 @@
+"use client"
 import { useState } from "react";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
-const defaultFormData = {
-    email: '',
-    name: '',
-    password: '',
-}
+
+
+
 const Auth = () => {
-    const [formData, setFormData] = useState(defaultFormData)
-    const handleInputChange = (e) => {
-        // Tomorrow i completing my work
-        console.log(e);
-    }
-    const inputStyles = 'border border-gray-300 sm:text-sm text-black rounded-lg block  w-full p-2.5 focus:outline-none';
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+      const form  = e.target;
+      const name = form.name.value;
+      console.log(name);
+  
+    };
+
+
+    const inputStyles = 'border border-gray-300 sm:text-sm text-black rounded-lg block w-full p-2.5 focus:outline-none';
+
     return (
         <section className="container mx-auto">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-80 md:w-[70%] mx-auto">
@@ -26,13 +31,42 @@ const Auth = () => {
                         <FcGoogle className="ml-3 text-4xl cursor-pointer" />
                     </span>
                 </div>
-                <form className="space-y-4 md:space-y-6">
-                    <input type="text" className={inputStyles} onChange={handleInputChange} value={formData.name} name="name" id="name" placeholder="Type Your Name" required />
-                    <input type="email" className={inputStyles} onChange={handleInputChange}  value={formData.email} name="email" id="email" placeholder="name@company.com" required />
-                    <input type="password" className={inputStyles} onChange={handleInputChange}  value={formData.password} minLength={6} name="password" id="password" placeholder="Type Your Password" required />
-                <button type="submit" className="w-full bg-tertiary-dark focus:outline-none font-medium rounded-lg text-sm px-5 text-center py-2.5">Sign Up</button>
+                <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
+                    <input
+                        type="text"
+                        className={inputStyles}
+                        name="name"
+                        id="name"
+                        placeholder="Type Your Name"
+                        required
+                    />
+                    <input
+                        type="email"
+                        className={inputStyles}
+                        name="email"
+                        id="email"
+                        placeholder="name@company.com"
+                        required
+                    />
+                    <input
+                        type="password"
+                        className={inputStyles}
+                        minLength={6}
+                        name="password"
+                        id="password"
+                        placeholder="Type Your Password"
+                        required
+                    />
+                    <button
+                        type="submit"
+                        className="w-full bg-tertiary-dark focus:outline-none font-medium rounded-lg text-sm px-5 text-center py-2.5"
+                    >
+                        Sign Up
+                    </button>
                 </form>
-                <button className="text-blue-700 underline">login</button>
+                <button type="button" className="text-blue-700 underline">
+                    Login
+                </button>
             </div>
         </section>
     );
